1380186e6f053fbbd6d2a0d078ff9f75a30aee4d
leighiam.virrey <virreyleighiam@gmail.com>
2022-02-28 16:27:57 -0700

    Added elevated permissions

--- Vroom.ps1
+++ Vroom.ps1
@@ -1,3 +1,19 @@
+param([switch]$Elevated)
+
+function Test-Admin {
+    $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+    $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
+
+if ((Test-Admin) -eq $false)  {
+    if ($elevated) {
+        # tried to elevate, did not work, aborting
+    } else {
+        Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
+    }
+    exit
+}
+
 #This gets the path of the DTImport OU in AD
 $OUDTImport = 'OU=DT Import,OU=CEI Users,DC=CEI,DC=EDU'
 #This gets the Destination OU of where we're sending the users
