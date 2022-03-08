59a0176f0e3e9b234b15b835e8d6a7c4c329b1e5
leighiam.virrey <virreyleighiam@gmail.com>
2022-03-08 14:29:29 -0700

    Added Input Validation

--- Vroom.ps1
+++ Vroom.ps1
@@ -4,6 +4,16 @@ function Test-Admin {
     $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
     $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
 }
+function DTImport_check {
+    if ($Total_users.count -eq 0){
+        Write-Host("There is no users inside DT Import, No reason to run the script") -BackgroundColor Red
+        break
+    }
+    else{
+        Write-Host("There is $($Total_users.count) account inside DT Import") -BackgroundColor Green -ForegroundColor Black
+    }
+    
+}
 
 if ((Test-Admin) -eq $false)  {
     if ($elevated) {
@@ -13,23 +23,26 @@ if ((Test-Admin) -eq $false)  {
     }
     exit
 }
-
-#This gets the path of the DTImport OU in AD
+$OU = @(Get-ADOrganizationalUnit -LDAPFilter '(name=*)' -SearchBase "OU=Students,OU=CEI Users,DC=CEI,DC=EDU" | ForEach-Object {$_.name})
 $OUDTImport = 'OU=DT Import,OU=CEI Users,DC=CEI,DC=EDU'
 $Total_users = Get-ADUser -Filter * -SearchBase $OUDTImport | Measure-Object
+$Lists_OU = Get-ADOrganizationalUnit -LDAPFilter '(name=*)' -SearchBase "OU=Students,OU=CEI Users,DC=CEI,DC=EDU" | Sort-Object -Property Name | Format-Table @{L='List of OU';E={$_.Name}}
+$Lists_OU
+DTImport_check
 
-if ($Total_users.count -eq 0){
-    Write-Host("There is no users inside DT Import, No reason to run the script") -BackgroundColor Red
-    break
+$OU_Exists = $false
+while ($OU_Exists -eq $false) {
+    $Academic_Year = Read-Host("Which OU do you want to send the accounts in? (Type one from the list)")
+    if ($Academic_Year -in $OU) {
+    $OU_Exists = $true
+    continue
+    }
+    else{
+        Write-Host("Not a valid choice") -BackgroundColor Red -ForegroundColor Black
+    }
 }
-else{
-    Write-Host("There is $($Total_users.count) account inside DT Import") -BackgroundColor Green -ForegroundColor Black
-}
-Get-ADOrganizationalUnit -LDAPFilter '(name=*)' -SearchBase "OU=Students,OU=CEI Users,DC=CEI,DC=EDU" | Sort-Object -Property Name | Format-Table @{L='List of OU';E={$_.Name}}
-$Academic_Year = Read-Host("Which OU do you want to send the accounts in? (Type one from the list)")
-#This gets the Destination OU of where we're sending the users
+
 $TargetOU = "OU=$($Academic_Year),OU=Students,OU=CEI Users,DC=CEI,DC=EDU"
-#This puts the users in the DTimport OU in a variable so we can loop through each student and change tags
 $DTList = Get-ADUser -Filter * -SearchBase $OUDTImport
 
 #This loops everything from the $DTList variable and then changes the description and company tags to the specified strings
@@ -39,7 +52,7 @@ foreach ($user in $DTList) {
     $counter ++
     Set-ADUser $user -Description $Academic_Year -Company "Student"
     Move-ADObject -Identity $user -TargetPath $TargetOU
-    Write-Host ("$($user.name) has been moved to Spring 2022 OU and student license has been set") -ForegroundColor Green
+    Write-Host ("$($user.name) has been moved to $Academic_Year OU and student license has been set") -ForegroundColor Green
     Write-Host ("$counter AD account has been moved")
 }
 Pause
