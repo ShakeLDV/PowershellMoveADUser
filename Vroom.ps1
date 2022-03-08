8878d390ac5268a4a5eb4db8b3404b191f02f76f
leighiam.virrey <virreyleighiam@gmail.com>
2022-03-08 12:26:06 -0700

    Added input so it can be used throughout the years

--- Vroom.ps1
+++ Vroom.ps1
@@ -16,13 +16,30 @@ if ((Test-Admin) -eq $false)  {
 
 #This gets the path of the DTImport OU in AD
 $OUDTImport = 'OU=DT Import,OU=CEI Users,DC=CEI,DC=EDU'
+$Total_users = Get-ADUser -Filter * -SearchBase $OUDTImport | Measure-Object
+
+if ($Total_users.count -eq 0){
+    Write-Host("There is no users inside DT Import, No reason to run the script") -BackgroundColor Red
+    break
+}
+else{
+    Write-Host("There is $($Total_users.count) account inside DT Import") -BackgroundColor Green -ForegroundColor Black
+}
+Get-ADOrganizationalUnit -LDAPFilter '(name=*)' -SearchBase "OU=Students,OU=CEI Users,DC=CEI,DC=EDU" | Sort-Object -Property Name | Format-Table @{L='List of OU';E={$_.Name}}
+$Academic_Year = Read-Host("Which OU do you want to send the accounts in? (Type one from the list)")
 #This gets the Destination OU of where we're sending the users
-$TargetOU = 'OU=Spring 2022,OU=Students,OU=CEI Users,DC=CEI,DC=EDU'
+$TargetOU = "OU=$($Academic_Year),OU=Students,OU=CEI Users,DC=CEI,DC=EDU"
 #This puts the users in the DTimport OU in a variable so we can loop through each student and change tags
 $DTList = Get-ADUser -Filter * -SearchBase $OUDTImport
 
 #This loops everything from the $DTList variable and then changes the description and company tags to the specified strings
+$counter = 0
+Clear-Host
 foreach ($user in $DTList) {
-    Set-ADUser $user -Description "Spring 2022" -Company "Student"
+    $counter ++
+    Set-ADUser $user -Description $Academic_Year -Company "Student"
     Move-ADObject -Identity $user -TargetPath $TargetOU
+    Write-Host ("$($user.name) has been moved to Spring 2022 OU and student license has been set") -ForegroundColor Green
+    Write-Host ("$counter AD account has been moved")
 }
+Pause
